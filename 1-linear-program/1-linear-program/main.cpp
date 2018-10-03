3ac18329bb7da5c16191853169cd6ce808f6a1af
Полина Стешина <polinastesina@MacBook-Air-Polina.local>
2018-10-03 23:02:12 +0300

    initial commit

--- 1-linear-program/1-linear-program/main.cpp
+++ 1-linear-program/1-linear-program/main.cpp
@@ -8,14 +8,10 @@ int main(){
     cin >> x;
     
     z1 = (pow (x, 2) + 2 * x - 3 + (x + 1) * sqrt(pow (x, 2) - 9)) /
-    (pow (x, 2) - 2 * x - 3 + (x - 1) * sqrt(pow (x, 2) - 9));
+         (pow (x, 2) - 2 * x - 3 + (x - 1) * sqrt(pow (x, 2) - 9));
     
     z2 = sqrt((x + 3) / (x - 3));
     
     cout << "z1 = " << z1 << endl << "z2 = " << z2 << endl;
     return 0;
 }
-
-
-
-
