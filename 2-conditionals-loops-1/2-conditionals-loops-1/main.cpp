2e549529383fdd78d363cc58dbddef5a130e69bc
Полина Стешина <polinastesina@MacBook-Air-Polina.local>
2018-10-09 21:00:04 +0300

    2,3

--- 2-conditionals-loops-1/2-conditionals-loops-1/main.cpp
+++ 2-conditionals-loops-1/2-conditionals-loops-1/main.cpp
@@ -24,33 +24,37 @@ int main () {
     cout << fixed;
     
     if (dx < kEps) {cout << "Error dx";}
-    if (c == 0) {cout << "Error ДЕЛ/0";}
-    if ((abs(dx) > kEps ) && (abs(c) > kEps))
     
+    if ((abs(dx) > kEps ) && (abs(c) > kEps))
     {cout << string(25, '-') << endl;
      cout << "|     X     |     F     |" << endl;
-     cout << string(25, '-') << endl;
+        cout << string(25, '-') << endl;
      cout.precision(3);
         
         while (x1 <= x2)
-            
-        {if ((abs(x1) > kEps) and (abs(b) < kEps))
+        {if (abs(x1) < kEps && abs(b) > kEps)
         {f = a * pow((x1 + c), 2) - b;
         }
-        else
-            if ((abs(x1) > kEps) and (abs(b) > kEps))
-        {f = (x1 - a) / -c;
-        }
+        else{
+            if (abs(x1) < kEps && abs(b) < kEps)
+            {if (c == 0) {cout << "Error divide by 0";}
             else
-        {f = a + x1 / c;
-        }
-            if (((int(a) % int(b)) & (int(a) | int(c))) != 0)
-            {cout << "|" << setw(6) << x1 << setw(6) << "|";
-             cout << setw(6) << f << setw(6) << "|" << endl;
-            }
+        {f = (x1 - a) / -c;
+        }}
+            else
+                
+            {if (c == 0) {cout << "Error divide by 0";}
+            else
+        {f = a + x1 / c;}
+                
+            }}
+            cout << "|" << setw(6) << x1 << setw(6) << "|";
+            if (((int(a) % int(b)) & !(int(a) | int(c))) != 0)
+            {cout << setw(6) << int (f) << setw(6) << "|" << endl;}
+            else
+            {cout << setw(6) << f << setw(6) << "|" << endl;}
             x1 += dx;
-        }
-            cout << string(25, '-') << endl;
-        }
+            cout << string(25, '-') << endl;}
+    }
     return 0;
-}
+    }
--- 3-conditionals-loops-2/3-conditionals-loops-2/main.cpp
+++ 3-conditionals-loops-2/3-conditionals-loops-2/main.cpp
@@ -6,53 +6,62 @@
 using namespace std;
 
 int main()
-    {double x, x2, dx, ln, eps, f;
-     const int max = 1000;
-        
-        cout << "x = ";
-        cin  >> x;
-        cout << "x2 = ";
-        cin  >> x2;
-        cout << "dx = ";
-        cin  >> dx;
-        cout << "eps = ";
-        cin  >> eps;
-
-        cout << string(73, '-') << endl;
-        cout << "|" << setw(9) << "x" << setw(9);
-        cout << "|" << setw(9) << "ln x built-in" << setw(2);
-        cout << "|" << setw(9) << "  ln x series ";
-        cout << "|" << setw(9) << "i" << setw(9)<< "|" << endl;
-        cout << string(73, '-') << endl;
-        
-        cout << fixed;
-        cout.precision(6);
-
-        while (x <= x2)
-        {cout << "|" << setw(11) << x << setw(7) << "|";
-            cout << setw(11) << f << setw(7) << "|";
-            ln = 0;
-            
-            for (int n = 1; n < max; n++)
-            {
-                f = (2 * pow((x --), (2 * n ++))) /
-                    (2 * n ++) * pow((x ++), (2 * n ++));
-                ln  += f;
-                
-                if (abs(f) < eps)
-                    break;
-            }
-            if (int n = max) {
-                cout << setw(11) << "Excess limit" << setw(6) << "|";
-                cout << setw(11) << " " << setw(7) << "|" << endl;
-            }
-            else
-            {
-                cout << setw(11) << ln << setw(7) << "|";
-                cout << setw(11) << n << setw(7) << "|" << endl;
-            }
-            x += dx;
-        }
-        cout << string(73, '-') << endl;
-        return 0;
+{
+    double x, x1, x2, dx, F, ln, eps;
+    const double kEps = 1e-15;
+    int n, max_iter = 1000;
+    
+    cout << "ln(x), x <= 0"<< endl;
+    cout << "x1 = ";
+    cin >> x1;
+    cout << "x2 = ";
+    cin >> x2;
+    cout << "dx = ";
+    cin >> dx;
+    cout << "eps = ";
+    cin >> eps;
+    
+    if ((dx < kEps && eps < kEps) && (x1 <= 0))
+    {
+        cout << "Input error" << endl;
     }
+    cout << string(73, '-') << endl;
+    cout << "|" << setw(9) << " x " << setw(9);
+    cout << "|" << setw(11) << " ln(x) " << setw(7);
+    cout << "|" << setw(9) << "  ln(x) series " << setw(3);
+    cout << "|" << setw(11) << " n " << setw(7)<< "|" << endl;
+    cout << string(73, '-') << endl;
+    
+    cout << fixed;
+    cout.precision(3);
+    
+    x = x1;
+    
+    while (x<=x2)
+    {
+        cout << "|" << setw(11) << x << setw(7) << "|";
+        cout << setw(11) << log(x) << setw(7) << "|";
+        ln = 0;
+        for (n = 0; n < max_iter; n++)
+        {
+            F = (2 * pow((x - 1), (2 * n + 1))) /
+            (2 * n + 1) * pow((x + 1), (2 * n + 1));
+            ln += F;
+            
+            if (abs(F) < eps)
+                break;
+        }
+        if (n == max_iter) {
+            cout << setw(11) << "Excess limit" << setw(6) << "|";
+            cout << setw(11) << " " << setw(7) << "|" << endl;
+        }
+        else
+        {
+            cout << setw(11) << ln << setw(7) << "|";
+            cout << setw(11) << n << setw(7) << "|" << endl;
+        }
+        x += dx;
+    }
+    cout << string(73, '-') << endl;
+    return 0;
+}
