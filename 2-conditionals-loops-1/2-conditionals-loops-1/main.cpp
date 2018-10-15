2eac9f3fb1fd4e3a80d3b4db920d24dfe22b4e58
Полина Стешина <polinastesina@MacBook-Air-Polina.local>
2018-10-15 10:19:11 +0300

    New

--- 2-conditionals-loops-1/2-conditionals-loops-1/main.cpp
+++ 2-conditionals-loops-1/2-conditionals-loops-1/main.cpp
@@ -5,54 +5,82 @@
 
 using namespace std;
 
-int main () {
-    double x1, x2, dx, a, b, c, f = 0;
+int main()
+{
     const double kEps = 1e-15;
     
+    double x1, x2, dx, a, b, c;
     cout << "a = ";
-    cin  >> a;
+    cin >> a;
     cout << "b = ";
-    cin  >> b;
+    cin >> b;
     cout << "c = ";
-    cin  >> c;
+    cin >> c;
     cout << "x1 = ";
-    cin  >> x1;
+    cin >> x1;
     cout << "x2 = ";
-    cin  >> x2;
+    cin >> x2;
     cout << "dx = ";
-    cin  >> dx;
-    cout << fixed;
+    cin >> dx;
     
-    if (dx < kEps) {cout << "Error dx";}
-    
-    {cout << string(25, '-') << endl;
-     cout << "|     X     |     F     |" << endl;
-        cout << string(25, '-') << endl;
-     cout.precision(3);
+    if (dx < kEps) { cout << "Error dx"; }
+    else
+    {
+        cout << string(26, '-') << endl;
+        cout << "|     X     |      F     |" << endl;
+        cout << string(26, '-') << endl;
         
+        cout << fixed;
+        cout.precision(3);
+        
+        double f;
         while (x1 <= x2)
-        {if (abs(x1) < kEps && abs(b) > kEps)
-        {f = a * pow((x1 + c), 2) - b;
-        }
-        else{
-            if (abs(x1) < kEps && abs(b) < kEps)
-            {if (c < kEps) {cout << "Error divide by 0";}
+        {
+            cout << "|" << setw(8) << x1 << setw(4) << "|";
+            
+            if (abs(x1) < kEps && !(abs(b) < kEps))
+            {
+                f = a * pow((x1 + c), 2) - b;
+            }
             else
-        {f = (x1 - a) / -c;
-        }}
+            {
+                if (abs(x1) < kEps && abs(b) < kEps)
+                {
+                    if (abs(c) < kEps)
+                    {
+                        cout << "  div by 0  |\n";
+                        x1 += dx;
+                        continue;
+                    }
+                    else
+                    {
+                        f = (x1 - a) / (-c);
+                    }
+                }
+                else
+                {
+                    if (abs(c) < kEps)
+                    {
+                        cout << "  div by 0  |\n";
+                        x1 += dx;
+                        continue;
+                    }
+                    else
+                    {
+                        f = a + x1 / c;
+                    }
+                }
+            }
+            
+            if (((int(a) ^ int(b)) & ~(int(a) | int(c))) != 0)
+                cout << setw(9) << f << setw(4) << "|" << endl;
             else
-                
-            {if (c < kEps) {cout << "Error divide by 0";}
-            else
-        {f = a + x1 / c;}
-            }}
-            cout << "|" << setw(6) << x1 << setw(6) << "|";
-            if (((int(a) ^ int(b)) & !(int(a) | int(c))) != 0)
-             {cout << setw(6) << f << setw(6) << "|" << endl;}
-            else
-             {cout << setw(6) << int (f) << setw(6) << "|" << endl;}
+                cout << setw(7) << int(f) << setw(6) << "|" << endl;
+            
             x1 += dx;
-            cout << string(25, '-') << endl;}
+        }
+        
+        cout << string(26, '-') << endl;
     }
     return 0;
-    }
+}
--- 6-arrays-2d/6-arrays-2d/main.cpp
+++ 6-arrays-2d/6-arrays-2d/main.cpp
@@ -1,15 +1,55 @@
-//
-//  main.cpp
-//  6-arrays-2d
-//
-//  Created by Полина Стешина on 14.10.2018.
-//  Copyright © 2018 Полина Стешина. All rights reserved.
-//
-
+#include "stdafx.h"
+#include "complex"
+#include "conio.h"
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+    float a[100][100];
+    int i,n,j,m,k,x; // i-счётчик cтрок j-счётчик столбцов
+    printf("Enter n, m ");
+    scanf("%i %i",&n, &m);
+    for (i=1;i<=n;i++)
+    {
+        for (j=1;j<=m;j++)
+        {
+            printf("\n a[%i,%i]= ",i,j);
+            scanf("%f",&a[i][j]);
+        }
+    }
+    for( i=1; i <=n; i++);   //cортировка массива
+    {
+        for( j=1; m-1<=j ; j++ ) ;
+        {
+            if ( a[j] > a[j+1] ); // внутренний цикл прохода
+            {
+                x=a[j+1];
+                a[j+1]=a[j];
+                a[j]=x;
+            }
+        }
+    }
+    
+    
+    for(int j=1;j<=m;j++)
+    {
+        bool ext=true;
+        for(int i=1;i<=n;i++)
+            if(a[i][j]<0)
+            {
+                ext=false;
+                break;
+            }
+        if(ext)cout<<"First column where all element >0 is "<<j+1;
+    }
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=m;j++)   printf("%f ",a[i][j]);
+        printf("\n");
+    }
+    _getch();
     return 0;
+    
 }
+
