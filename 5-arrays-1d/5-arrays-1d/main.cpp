02162e9ff8f3bcb5364ebd2525fcbc255bffa6ce
Полина Стешина <polinastesina@MacBook-Air-Polina.local>
2018-11-08 19:10:57 +0300

    instructions 3

--- 5-arrays-1d/5-arrays-1d/main.cpp
+++ 5-arrays-1d/5-arrays-1d/main.cpp
@@ -1,15 +1,15 @@
-#include<iostream>
-#include<cmath>
+#include <iostream>
+#include <cmath>
 
 using namespace std;
 
 int main() {
-    int n, k = 0, index = 0;
-    double min,sum = 0;
+    
     const double kEps = 1e-15;
+    int n;
     cout << "n = ";
     cin  >> n;
-    double a[n], trans;
+    double a[n];
     if (n <= 0)
     {cout << "Input error" << endl;
         return 1;}
@@ -18,13 +18,16 @@ int main() {
     for(int i = 0; i < n; i++)
     {cin >> a[i];}
     
-    
+    // Кол-во отрицательных элементов массива
+    int k = 0;
     for (int i = 0; i < n; i++)
         if (a[i] < kEps) k++;
-    {cout << k << endl;}
+        {cout << k << endl;}
     
-
+    // Сумма модулей элементов массива, расположенных после минимального по модулю элемента
+    double min,sum = 0;
     min = abs(a[0]);
+    int index = 0;
     for(int i = 0; i < n; i++)
     if(abs(a[i]) < min)
     {min = abs(a[i]); index = i;}
@@ -33,17 +36,18 @@ int main() {
     {sum += abs(a[i]);}
     cout << sum << endl;
     
-   
+    // Замена всех отрицательных элементов массива их квадратами
     for(int i = 0; i < n; i++)
         if(a[i] < kEps)
         {a[i] *= a[i];}
     
+    double buf;
     for(int i = 0; i < (n - 1); i++)
         for(int g = i + 1; g < n; g++)
             if(a[i] > a[g])
-            {trans = a[i];
+            {buf = a[i];
              a[i] = a[g];
-             a[g] = trans;}
+             a[g] = buf;}
                 
                 for(int i = 0; i < n; i++)
                 {cout << a[i] << ' ';}
--- 7-strings/7-strings/main.cpp
+++ 7-strings/7-strings/main.cpp
@@ -13,7 +13,7 @@ int main()
     file_name += ".txt";
     ifstream fin(file_name);
     
-    if (!fin.is_open())
+    if (!fin)
     {
         cout << "can't open "<< file_name << endl;
         return 1;
@@ -24,32 +24,32 @@ int main()
     fin.seekg (0, ios::beg);
     fin.read (buf, len);
     buf [len] = '\0';
-    long n = 0, i = 0, j = 0, w = 0, e = 0, r = 0;
+    long a = 0, i = 0, j = 0, b = 0, c = 0, d = 0;
     
     while (buf [i])
-    {
+     {
         if (buf [i] == '?')
-        {for (j = n; j <= i; j++)
+        {for (j = a; j <= i; j++)
             cout << buf [j];
-            n = i + 1;
+            a = i + 1;
         }
         if (buf [i] == '!' || buf [i] == '.')
-            n = i + 1;
+            a = i + 1;
         i++;
-    }
+     }
     cout << endl;
-    while (buf [e])
-    {
-        if (buf [e] == '!')
-        {
-            for ( r = w; r <= e; r++)
-                cout << buf [r];
-            w = e + 1;
-        }
-        if ( buf [e] == '?' || buf [e] == '.')
-            w = e + 1;
-        e++;
-    }
+    while (buf [c])
+      {
+        if (buf [c] == '!')
+         {
+            for ( d = b; d <= c; d++)
+                cout << buf [d];
+                b = c + 1;
+         }
+        if ( buf [c] == '?' || buf [c] == '.')
+            b = c + 1;
+            c++;
+      }
     return 0;
 }
 
